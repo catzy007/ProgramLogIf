b01d2bb29115c687e0405575e51c57e6c770390a
catzy007 <catzy.rais@gmail.com>
2019-04-02 16:19:04 +0700

    add so many f*ckin exceptions

--- modustollens.js
+++ modustollens.js
@@ -10,15 +10,31 @@
 		var premis2x = globalRemover(premis2, 'tidak benar ');
 		var premis2x = globalRemover(premis2x, 'tidak ');
 		
-		//console.log(premis1q); //debug_line_can_be_removed
-		//console.log(premis2x); //debug_line_can_be_removed
+		//hapus kata 'tidak' atau 'tidak benar' pada 'premis 1 q' 
+		var premis1qx = globalRemover(globalGetQ(premis1, ' maka '), 'tidak benar ');
+		var premis1qx = globalRemover(premis1qx, 'tidak ');
+
 		
+		console.clear();
+		console.log('P1      = ' + premis1);
+		console.log('P1 Q    = ' + premis1q);
+		console.log('P2      = ' + premis2);
+		console.log('P1 Q X  = ' + premis1qx);
+		console.log('P2   X  = ' + premis2x);
+		console.log('\"' + premis1q + '\" tidak sama dengan \"' + premis2 + '\"');
+		console.log('\"' + premis1qx + '\" sama dengan \"' + premis2x + '\"');
+		
+
 		//jika premis1p sama dengan premis2x dan jika premis2 merupakan negasi
-		if(globalValidator(premis1, ' maka ') 
-			&& premis1q == premis2x 
-			&& globalValidator(premis2, 'tidak ')
+		if(globalValidator(premis1, ' maka ')	//'premis 1 q' harus berupa 'maka'
+			&& premis1q != premis2				//'premis 1 q' tak boleh sama degan 'premis 2'
+			&& premis1qx == premis2x			//'premis 1 q' yang telah diproses harus sama dengan 'premis 2' yang telah diproses
 		){
-			document.getElementById("konklusi").innerHTML = modusTollensNegasiP(premis1);
+			if(globalValidator(premis1q, 'tidak ') && globalValidator(premis2, 'tidak ')){
+				document.getElementById("konklusi").innerHTML = "Tidak Valid!";
+			}else{
+				document.getElementById("konklusi").innerHTML = modusTollensNegasiP(premis1);
+			}
 		}else{
 			document.getElementById("konklusi").innerHTML = "Tidak Valid!";
 		}
